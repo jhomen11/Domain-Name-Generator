a0b11fcbd76868de50df43bee81ad5d2468e7d7b
Jhonny Mendoza <jhonnymendoza@Mac-mini-de-Jhonny.local>
2021-10-22 17:49:24 -0300

    Domain Name Generator

---
+++ apps.js
@@ -0,0 +1,22 @@
+ var pronoun = ['the','our'];
+ var adj = ['great', 'big' ];
+ var noun = ['jogger','racoon'];
+
+  
+    
+    for(i = 0; i < pronoun.length; i++){
+        for(j = 0; j < adj.length; j++){
+            for(k = 0; k < noun.length; k++){
+                var dominio  = pronoun[i]+adj[j]+noun[k]+".com";
+                console.log(dominio);
+            }
+        } 
+    }
+    
+     
+    
+    
+    
+
+
+
